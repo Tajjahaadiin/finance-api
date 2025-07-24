8b56165a4fb3060c0949de40337d7ca287e8ed16
Tajjuddiin Auliya Ahaadiin <tajjuddiinauliya@gmail.com>
2025-07-24 11:41:23 +0700

    fix: Resolve database connection in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -40,20 +40,14 @@ module.exports = {
 
   production: {
     client: "pg",
-    connection: {
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-      database: process.env.DB_NAME,
-      password: process.env.DB_PASSWORD,
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
       tableName: "knex_migrations",
-      directory: ".db/migrations",
+      directory: "./db/migrations",
     },
     seeds: {
       directory: "./db/seeds",
